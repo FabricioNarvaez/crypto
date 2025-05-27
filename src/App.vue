8ed9b0e9a6eaa035ac1ddd0b10b3aaa4ad2f50fc
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-05-27 11:50:19 +0200

    Created coin selector

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
-<script setup>
-</script>
-
 <template>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">-- Seleccione --</option>
+            <option v-for="moneda in monedas" :key="moneda.codigo" :value="moneda.codigo">
+              {{ moneda.texto }}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script setup>
+  import { ref } from 'vue';
+
+  const monedas = ref([
+    { codigo: 'USD', texto: 'Dólar Estadounidense'},
+    { codigo: 'MXN', texto: 'Peso Mexicano' },
+    { codigo: 'EUR', texto: 'Euro' },
+    { codigo: 'GBP', texto: 'Libra Esterlina' }
+  ])
+</script>
